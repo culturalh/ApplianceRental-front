971a674b99ceb73edfd5217097276c406de50591
culturalH <1601331821@qq.com>
2024-12-22 19:20:06 +0800

    集成ant design vue 及其图标

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
+import * as Icons from '@ant-design/icons-vue'
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(Antd).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router).use(Antd)
+app.mount('#app')
+
+// 全局使用图标，遍历引入
+const icons = Icons
+for (const i in icons) {
+    app.component(i, icons[i])
+}
